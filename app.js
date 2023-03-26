15b0ea47287dfce9835236e3188792bff0437d31
Gerardo De Los Santos Juarez <gerardo.delossantos19@sistemas.tecsanpedro.edu.mx>
2023-03-25 19:17:43 -0600

    Cambios en el diseño

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 
-const menu = document.querySelectorAll('#navegacion div ul li a');
+const menu = document.querySelectorAll('.navegacion ul li a');
 
+console.log(menu);
 const secciones = document.querySelectorAll('main section');
 
 
 
+
+// Ocultar y mostrar secciones
 let elementoSeleccionado ='';
 
 const perfil = document.querySelector('.section_profile');
@@ -16,7 +19,7 @@ const certificacion = document.querySelector('.section_certificates');
 
 let map = new Map();
 
-map.set('Perfil',perfil)
+map.set('Acerca de mí',perfil)
    .set('Habilidades',habilidades)
    .set('Experiencia',experiencia)
    .set('Portafolio',portafolio)
@@ -32,15 +35,38 @@ function cambiarSeccion(e){
 
     const seleccion = e.target;
     let seccionSeleccionada = map.get(seleccion.textContent);
+    console.log(seccionSeleccionada);
     if(seccionSeleccionada.classList.contains('no-mostrar')){
         seccionSeleccionada.classList.remove('no-mostrar');
+        
     }
     menu.forEach(elemento =>{
+        console.log(elemento.textContent);
+        console.log(seleccion.textContent)
         if(elemento.textContent !== seleccion.textContent){
             let otros = map.get(elemento.textContent);
+            console.log(otros);
             if(!otros.classList.contains('no-mostrar')){
                 otros.classList.add('no-mostrar');
+                
             }
         }
     });
+    subrayar(e);
 };
+
+// Añadir clase de subrayado al enlace
+function subrayar(e){
+    
+    const enlace = e.target;
+    menu.forEach(seccion =>{
+        if(seccion.classList.contains('subrayado')){
+            seccion.classList.remove('subrayado');
+        }
+    })
+
+    if(!enlace.classList.contains('subrayado')){
+        enlace.classList.add('subrayado');
+    }
+    
+}
